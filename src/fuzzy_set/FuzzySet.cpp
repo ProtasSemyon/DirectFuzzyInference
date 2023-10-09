b1a2784bff37617d5929e8668725821316ac1360
Saymon <smn1235463782@gmail.com>
2023-10-09 08:54:01 +0300

    Add FuzzySet

--- src/fuzzy_set/FuzzySet.cpp
+++ src/fuzzy_set/FuzzySet.cpp
@@ -1,1 +1,29 @@
 #include "FuzzySet.hpp"
+#include <iostream>
+
+FuzzySet::FuzzySet(std::string name, const std::map<std::string, double, std::less<>> & data) : data(data), setName(std::move(name)) {
+}
+
+void FuzzySet::addElement(const std::string& key, double value) {
+  data[key] = value;
+}
+
+std::ostream& operator<<(std::ostream& os, const FuzzySet& set) {
+  os << set.setName;
+  os << " = { ";
+
+  size_t index = 0;
+  const size_t setSize = set.data.size();
+  for (const auto &[key, value] : set.data) {
+    os << "( " << key << ", " << value << " )";
+    if (index < setSize - 1) {
+      os << ", ";
+    }
+    index++;
+  }
+
+  os << " }" << std::endl;
+  return os; 
+}
+
+
--- src/fuzzy_set/FuzzySet.hpp
+++ src/fuzzy_set/FuzzySet.hpp
@@ -5,8 +5,16 @@
 
 class FuzzySet {
 public:
+  FuzzySet() = default;
+
+  explicit FuzzySet(std::string name, const std::map<std::string, double, std::less<>> & data);
+
   void addElement(const std::string& key, double value);
+
+  friend std::ostream& operator<<(std::ostream& os, const FuzzySet& set);
   
 private:
-  std::map<std::string, double> data;
+  std::map<std::string, double, std::less<>> data;
+
+  std::string setName;
 };
